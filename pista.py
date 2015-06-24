ae2734fa49b5e51c1c052261f8f79cca5938946b
jloaiza018 <jorgeloaiza12@hotmail.com>
2015-06-24 23:45:27 +0000

    Borre todo!

--- pista.py
+++
@@ -1,6 +0,0 @@
-Deben buscar una forma de sincronizar los archivos que se borraron en github. Yo tampoco
-lo se hacer xD
-
-la solucion no deberia ser usar "git clone url" debe haber una forma distinta....
-
-el que logre esto, diga como lo hizo para que aprendamos todos!
