237506c8b576a58563e3d81ea9306adec1bdb822
user <email@4geeks.co@4geeks.co>
2015-06-24 16:54:47 +0000

    Actualice el archivo pista.

--- pista.py
+++ pista.py
@@ -1,2 +1,6 @@
 Deben buscar una forma de sincronizar los archivos que se borraron en github. Yo tampoco
 lo se hacer xD
+
+la solucion no deberia ser usar "git clone url" debe haber una forma distinta....
+
+el que logre esto, diga como lo hizo para que aprendamos todos!
